e7f2512236dbd78430f717d48e5b8dc70269c025
Andriel Chaoti <accounts@vandesdelca32.net>
2019-06-11 15:15:21 -0600

    Rewrite the entire thing to make it *a lot* better.

--- healthCommand.js
+++ healthCommand.js
@@ -1,45 +1,94 @@
 const HEALTH_DESC = [ 'Unconcious', 'Near Death', 'Badly Injured', 'Injured', 'Barely Injured', 'Uninjured', 'Dead' ];
+var health_status = (function(){
+    return {
+        config: {
+            'health_attrib': 'bar1_value',
+            'health_max_attrib': 'bar1_max'
+        },
+
+        /**
+        * find and return the token object for a given token ID.
+        */
+        getTokenTarget: function(token_id) {
+            // this should only return one object, buttttttttttttt...
+            var tokens = findObjs({type: 'graphic', subtype: 'token', id: token_id}, true);
+            if(tokens.length === 0) return null; // invalid ID/target doesn't exist/clicked on the map.
+            return tokens[0]; // return the first token found (it's probably the one we want)
+        },
+
+        /**
+        * checks if the submitted token has number/non-empty values for supplied attributes.
+        */
+        checkTokenHealth: function(token) {
+            var current = token.get(this.config.health_attrib);
+            var max = token.get(this.config.health_max_attrib);
+
+            if (current === '' || max === '' || isNaN(current) || isNaN(max)) {
+                return null;
+            }
+            return current / max;
+        },
+
+        respond: function(content) {
+            // build the output message to send:
+            var output = '/direct \
+            <div style="background-color: #ffffee; border-style: solid; border-right-style: none; border-width: 1px; border-left-width: 12px; border-radius: 5px 0px 0px 5px; border-color: #b3b300; border-left-color: #aa80ff; padding: 10px;">' +
+            content +
+            '</div>';
+
+            sendChat('Health Info', output, null, {noarchive: true});
+        }
+
+
+    };
+
+
+})();
 
 on('chat:message', function(msg) {
-    // custom api macros
-    if(msg.type == 'api') {
+    if (msg.type == 'api') {
         if (msg.content.indexOf('!hp ') !== -1) {
-            // get target
+            // trim command off:
             var target = msg.content.replace('!hp ', '');
-            var tokens = findObjs({ type: 'graphic', subtype: 'token', id: target }, true);
-            if (tokens.length == 0) {
-                respond(msg, 'Can\'t find ' + target + '.');
-            } else {
-                // get bar value
-                var bar1_percent = (tokens[0].get('bar1_value') / tokens[0].get('bar1_max')) * 5;
-                // clamp bar value
-                bar1_percent = Math.min(Math.max(Math.ceil(bar1_percent), 0), 5);
-                var healthStr = HEALTH_DESC[bar1_percent];
-                if (tokens[0].get('status_dead')) healthStr = HEALTH_DESC[6];
-                var x = tokens[0].get('left');
-                var y = tokens[0].get('top');
-                var pageid = Campaign().get('playerspecificpages');
-                if (pageid == false) { 
-                    pageid = Campaign().get('playerpageid');
-                } else {
-                    pageid = pageid[msg.playerid];
+            // subcommands:
+            /* Make this whisper the person that sent it.
+            if (target.indexOf('config') !== -1) {
+                // show the current config if the user is gm
+                if(!playerIsGM(msg.playerid)) {
+                    return;
                 }
-                sendPing(x, y, Campaign().get('playerpageid'), null, false);
-                respond(msg, tokens[0].get('name') + ' is **' + healthStr + '**');
+                health_status.respond('<b><big>Current Config:</big></b><br />\
+                    Attribute: <code>'+health_status.config.health_attrib+'</code><br />\
+                    Max Attribute: <code>'+health_status.config.health_max_attrib+'</code><br />\
+                    <br /><b>Setting is currently not supported!</b>');
+                    return;
+            }*/
+
+            // handle the output!
+            var tokenObj = health_status.getTokenTarget(target);
+            if (tokenObj === null) health_status.respond('No target found<br /><br />ID: </b>'+target+'</b>');
+
+            var health_val = health_status.checkTokenHealth(tokenObj);
+            if (health_val === null) health_status.respond('Who the what now? Your token doesn\'t have the right stats.');
+
+            // check if the token is marked as dead or not and get proper value
+            var health_str;
+            if (tokenObj.get('status_dead')) {
+                health_str = HEALTH_DESC[6];
             }
+            else {
+                health_str = HEALTH_DESC[(Math.max(Math.min(Math.ceil(health_val * 5), 5), 0))];
+            }
+            health_status.respond('<b>'+tokenObj.get('name')+'</b> is <b>'+health_str+'</b>');
+
+            //ping the token in question so people know which one is being referred to.
+            var pageid = Campaign().get('playerspecificpages');
+            if (pageid === false) {
+                pageid = Campaign().get('playerpageid');
+            } else {
+                pageid = pageid[msg.playerid];
+            }
+            sendPing(tokenObj.get('left'), tokenObj.get('top'), pageid, null, false);
         }
     }
-})
-
-function respond(msg, content) {
-    var characters = findObjs({_type: 'character'});
-    var speaking;
-    characters.forEach(function(chr) { if(chr.get('name') == msg.who) speaking = chr; });
- 
-    var out;
-
-    out = '&{template:desc} {{desc=' + content + '}}'
-
-    if(speaking) sendChat('character|'+speaking.id, out, null, {noarchive: true});
-    else sendChat('player|'+msg.playerid, out, null, {noarchive: true});
-}
+});
